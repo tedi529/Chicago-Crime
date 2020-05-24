f09562d7ec3d024ec6e62983e1b2549a59eae54e
TLatinska <teodoralatinska@hotmail.com>
2020-05-24 12:06:31 -0400

    add MLP pickle model

--- app.py
+++ app.py
@@ -1,3 +1,36 @@
 import numpy as np
 import pandas as pd
+import json
+import ast
 from flask import Flask, request, jsonify, render_template
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("Machine_Learning/RF_model.sav")
+
+locmap = eval(open("Resources/loc_dict.txt").read())
+typemap = eval(open("Resources/type_dict.txt").read())
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    for value in ______:
+        return locmap.get(value)
+
+    for value in     
+        
+
+@app.route('/results',methods=['POST'])    
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
